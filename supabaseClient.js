55377b0c565e03f3c91dccfaf69e741cd339d031
usakowhity <usakowhity@yahoo.co.jp>
2026-01-22 00:23:26 +0900

    supabaseClient.js を削除

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js';
-
-export const supabase = createClient(
-  'https://lcpkqaoaqsotjaydukxs.supabase.co',
-  'sb_publishable__fsG5IT4g30OLUOKchn79g_W4vFv6k8'
-);
