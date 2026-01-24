a508948362b301048b69d85af982f12a36619efb
usakowhity <usakowhity@yahoo.co.jp>
2026-01-24 22:51:34 +0900

    auth.html , supabaseClient.js修正

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,8 @@
-const supabaseClient = supabase.createClient(
+// Supabase ライブラリを CDN から読み込む
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+// Supabase クライアントを作成
+const supabase = createClient(
   "https://lcpkqaoaqsotjaydukxs.supabase.co",
   "sb_publishable__fsG5IT4g30OLUOKchn79g_W4vFv6k8"
 );
